3124dfe0c580e903cd18c7a443ab1ec4f3515bf6
StephenHardin <35747868+StephenHardin@users.noreply.github.com>
2021-09-16 17:44:22 -0400

    Add files via upload

--- submit_all.py
+++ submit_all.py
@@ -3,7 +3,7 @@ import subprocess
 jobs = len(open("Datacard_list.txt").readlines(  ))
 
 #range is number of datacards
-for count in range(jobs) :
+for count in range(1,jobs+1) :
     count += 1
     subprocess.call('sbatch Ntuple_' + str(count) + '.sh', shell=True)
 
