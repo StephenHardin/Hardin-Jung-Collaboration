e035cb23f1df7d278483b9e28f619def1123a77e
StephenHardin <35747868+StephenHardin@users.noreply.github.com>
2021-09-16 17:13:10 -0400

    Add files via upload

--- make_jobs - Copy.py
+++ make_jobs - Copy.py
@@ -17,7 +17,7 @@ for line in lines :
         cfg.write("\n")
         cfg.write("#SBATCH --time=00:15:00")
         cfg.write("\n")
-        cfg.write("cd /home/hardin24/CMSSW_10_2_13/src/")
+        cfg.write("cd /home/hardin24/CMSSW_10_2_13/src/Hardin-Jung-Collaboration")
         cfg.write("\n")
         cfg.write("source /cvmfs/cms.cern.ch/cmsset_default.sh")
         cfg.write("\n")
